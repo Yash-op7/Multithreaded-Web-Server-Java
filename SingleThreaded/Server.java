df8985202c231f7b523cd7e87b78f24ed04d78a5
yash-op7 <yashm3375@gmail.com>
2024-08-13 19:49:53 +0530

    finished server.java code

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,3 +1,4 @@
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.PrintWriter;
@@ -5,27 +6,32 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 public class Server {
 
     public void run() {
-        int port 8010;
+        int port = 8010;
         ServerSocket socket = new ServerSocket(port);
         socket.setSoTimeout(10000);
-        while(true) {
-            try{
-                System.out.println("Server is listening of port "+ port);
-            Socket acceptedConnection = socket.accept();
-            System.out.printlin("Connection accepted from client " + acceptedConnection.getRemoteSocketAddress());
-            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream())
-            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+        while (true) {
+            try {
+                System.out.println("Server is listening of port " + port);
+                Socket acceptedConnection = socket.accept();
+                System.out.println("Connection accepted from client " + acceptedConnection.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                toClient.println("Hello from the server");
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
     }
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.run();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
