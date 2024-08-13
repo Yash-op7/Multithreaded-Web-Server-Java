1d37318f515d4060e6ce7783c1a4a6be5773d90a
yash-op7 <yashm3375@gmail.com>
2024-08-13 19:45:28 +0530

    setting up input and output stream

---
+++ SingleThreaded/Server.java
@@ -0,0 +1,31 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Server {
+
+    public void run() {
+        int port 8010;
+        ServerSocket socket = new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        while(true) {
+            try{
+                System.out.println("Server is listening of port "+ port);
+            Socket acceptedConnection = socket.accept();
+            System.out.printlin("Connection accepted from client " + acceptedConnection.getRemoteSocketAddress());
+            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream())
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+
+    }
+}
