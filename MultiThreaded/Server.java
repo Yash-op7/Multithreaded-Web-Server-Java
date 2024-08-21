14b81e6b89b3fca1e594564e09e9cbf215a22e14
yash-op7 <yashm3375@gmail.com>
2024-08-21 06:55:47 +0530

    Compeleted SingleThreaded Client and Server side code, Next is to write the Multithreaded Server

---
+++ MultiThreaded/Server.java
@@ -0,0 +1,14 @@
+
+public class Server {
+    
+    
+    public static void main(String[] args) {
+        int port = 8010;
+        Server server = new Server();
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+    
+}
