8be2046d99e509e392d0c5df0210092b06d85db8
yash-op7 <yashm3375@gmail.com>
2024-08-21 07:16:34 +0530

    Completed Client.java

--- MultiThreaded/Client.java
+++ MultiThreaded/Client.java
@@ -1,24 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-    public Runnable getRunnable() {
+    
+    public Runnable getRunnable() throws UnknownHostException, IOException {
         // Runnable is a functional interface
         return new Runnable() {
             @Override
             public void run() {
                 int port = 8010;
+                try {
+                    InetAddress address = InetAddress.getByName("localhost");
+                    Socket socket = new Socket(address, port);
+                    try (
+                        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+                    ) {
+                        toSocket.println("Hello from Client " + socket.getLocalSocketAddress());
+                        String line = fromSocket.readLine();
+                        System.out.println("Response from Server " + line);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // The socket will be closed automatically when leaving the try-with-resources block
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
             }
-        }
+        };
     }
-
+    
     // unlike the SingleThreaded Server this will spawn 100 clients for now few milliseconds apart
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Client client = new Client();
-        for(int i=0;i<100;i++) {
+        for(int i=0; i<100; i++){
             try{
                 Thread thread = new Thread(client.getRunnable());
                 thread.start();
-            } catch(Exception e) {
+            }catch(Exception ex){
                 return;
             }
         }
+        return;
     }
 }
