7cb952332c37e5e881217ee7e5f0eefc214588ee
yash-op7 <yashm3375@gmail.com>
2024-08-21 07:04:05 +0530

    Defining Customer functional interface

--- MultiThreaded/Server.java
+++ MultiThreaded/Server.java
@@ -1,12 +1,35 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.function.Consumer;
+
 
 public class Server {
     
+    public Consumer<Socket> getConsumer() {     // functional interface, because we need to pass it in a function and these are first class citizens so they are treated the same as varaibles
+    // returns a lambda, consumer interface doesn't return anything
+    return (clientSocket) -> {
+        try {
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+            
+        } catch (IOException e) {
+        }
+    }
+    }
     
     public static void main(String[] args) {
         int port = 8010;
         Server server = new Server();
         try {
-            
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.getSoTimeout(10000);
+            System.out.println("Server is listenign on port " + port);
+            while (true) {  
+                Socket accpetedSocket = serverSocket.accept();
+                Thread thread = new Thread();       // runs a function which communicates with the corresponding connected client, associated with the new connection created
+
+            }
         } catch (Exception e) {
         }
     }
